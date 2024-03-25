fc0fa30c478f84cadfc4537509348dda46a9ff93
Dominik <dforstmaier@gmail.com>
2024-03-25 10:57:46 +0100

    Created Statistics page with timebox usages

---
+++ TimeSlice.WebApp/TimeSlice.WebApp/Components/Pages/Statistics/TimeboxUsageEntry.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TimeSlice.WebApp.Components.Pages.Statistics
+{
+    public class TimeboxUsageEntry
+    {
+        public string Code { get; set; } = string.Empty;
+        public TimeSpan Duration { get; set; }
+    }
+}
